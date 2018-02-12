0b2f75b888065e9382405d4358b7974611f7cc11
Meijstro <35121706+Meijstro@users.noreply.github.com>
2018-02-12 14:15:26 +0100

    [W5-008] completed

--- all.php
+++ all.php
@@ -16,12 +16,35 @@
     <li><button id="cryptobtn">Cryptovaluta</button></li>
     <li><button id="trumpbtn">Trump</button></li>
   </ul>
-  <br> <br>
-<div>
+  <br>
+<div class=articleform>
   <form class="form" action="" method="POST">
     <h2 class="header2"> Zoek in articles </h2>
     <input id="zoekterm" type="text" name="zoekterm" required><br><br>
     <input class="button" type="submit" name= "button2" value="Search">
+    <input class="button" type="button" value="Reset" onclick="window.location.href='all.php'">
+  </form>
+</div>
+
+<div class=monthform>
+  <form class="form" action="" method="POST">
+    <h2 class="header2"> Zoek op maand </h2>
+    <select class="dropdown" name="maand" onchange="">
+      <option value="01">Januari</option>
+      <option value="02">Februari</option>
+      <option value="03">Maart</option>
+      <option value="04">April</option>
+      <option value="05">Mei</option>
+      <option value="06">Juni</option>
+      <option value="07">Juli</option>
+      <option value="08">Augustus</option>
+      <option value="09">September</option>
+      <option value="10">Oktober</option>
+      <option value="11">November</option>
+      <option value="12">December</option>
+    </select> <br> <br>
+    <input class="button" type="submit" name= "button3" value="Search">
+    <input class="button" type="button" value="Reset" onclick="window.location.href='all.php'">
   </form>
 </div>
 
@@ -41,6 +64,22 @@
            '<input class="button" type="submit" value="Reageer">'.
            '<input type="hidden" name="article_id" value="'.$row['id'].'">'."</form>"."<hr></div>";
     }
+  }
+    elseif (isset($_POST['button3'])) {
+            $maand = $_POST['maand'];
+
+// use data from the database and show it
+$sql= "SELECT * FROM articles WHERE timestamp LIKE '_____$maand%' ORDER BY timestamp DESC;";
+$result= mysqli_query($connection,$sql);
+while($row= mysqli_fetch_assoc($result))
+{
+echo "<div class='category_".$row['categorie']."'><br>"."<b>".$row["user"]."</b>"." "."<span>".$row["timestamp"].
+ "</span>"."<br>"."<br>"."&nbsp;&nbsp;".$row["message"]."<br>"."<br>".
+ '<form class="form" action="comment.php" method="POST">'.
+ '<input class="button" type="submit" value="Reageer">'.
+ '<input type="hidden" name="article_id" value="'.$row['id'].'">'."</form>"."<hr></div>";
+}
+
   }
   else{
 
